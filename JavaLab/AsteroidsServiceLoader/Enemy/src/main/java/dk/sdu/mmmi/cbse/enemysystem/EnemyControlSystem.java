d1493bf2a3ddb919ef0a136cad015d243989b8b0
LakeExhaust <45452219+ZeroExhaust@users.noreply.github.com>
2021-04-06 13:24:08 +0200

    Uploading af JavaLab
    Lavet af Lakeesan SDU CBSE

---
+++ JavaLab/AsteroidsServiceLoader/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyControlSystem.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.sdu.mmmi.cbse.enemysystem;
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import static dk.sdu.mmmi.cbse.common.data.GameKeys.LEFT;
+import static dk.sdu.mmmi.cbse.common.data.GameKeys.RIGHT;
+import static dk.sdu.mmmi.cbse.common.data.GameKeys.UP;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import java.util.Random;
+/**
+ *
+ * @author lake
+ */
+public class EnemyControlSystem  implements IEntityProcessingService {
+Random randomGenerator = new Random();
+
+    @Override
+    public void process(GameData gameData, World world) {
+  
+
+ 
+        for (Entity enemy : world.getEntities(Enemy.class)) {
+            PositionPart positionPart = enemy.getPart(PositionPart.class);
+            MovingPart movingPart = enemy.getPart(MovingPart.class);
+            int RandomNumber = randomGenerator.nextInt(3);
+            switch(RandomNumber) {
+              
+                case 0:
+                movingPart.setUp(true);
+               break;
+               
+               case 1:  
+              movingPart.setUp(true);
+              movingPart.setLeft(true);
+              break;    
+              
+               case 2:  
+              movingPart.setUp(true);
+              movingPart.setRight(true);
+              break;    
+            }
+         
+            positionPart.setPosition(35, 35);
+            movingPart.process(gameData, enemy);
+            positionPart.process(gameData, enemy);
+
+            updateShape(enemy);
+        }
+    }
+    
+    private void updateShape(Entity entity) {
+        float[] shapex = entity.getShapeX();
+        float[] shapey = entity.getShapeY();
+        PositionPart positionPart = entity.getPart(PositionPart.class);
+        float x = positionPart.getX();
+        float y = positionPart.getY();
+        float radians = positionPart.getRadians();
+
+        shapex[0] = (float) (x + Math.cos(radians) * 8);
+        shapey[0] = (float) (y + Math.sin(radians) * 8);
+
+        shapex[1] = (float) (x + Math.cos(radians - 4 * 3.1415f / 5) * 8);
+        shapey[1] = (float) (y + Math.sin(radians - 4 * 3.1145f / 5) * 8);
+
+        shapex[2] = (float) (x + Math.cos(radians + 3.1415f) * 5);
+        shapey[2] = (float) (y + Math.sin(radians + 3.1415f) * 5);
+
+        shapex[3] = (float) (x + Math.cos(radians + 4 * 3.1415f / 5) * 8);
+        shapey[3] = (float) (y + Math.sin(radians + 4 * 3.1415f / 5) * 8);
+
+        entity.setShapeX(shapex);
+        entity.setShapeY(shapey);
+    }
+    
+}
